95022b7cd17771ae5a50433f0b5ffda8f6c807a0
XITRIX <xitrix@bk.ru>
2018-05-29 00:56:54 +0300

    Improvements, fixes
    Added Boost libs inside the project
    Reorgonised code files
    Fixed "spacebar in folder name crash"
    Real file date creation send

---
+++ FTP/include/ftp/stdafx.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#ifdef _WIN32
+#include <SDKDDKVer.h>
+#include <tchar.h>
+#endif /* _WIN32 */
+
+#include <string>
+#include <iostream>
+#include <strstream>
+
+#include <sys/stat.h>
+#include <time.h>
+#include <stdio.h>
+
+#include <boost/asio.hpp>
+#include <boost/bind.hpp>
+#include <boost/function.hpp>
+#include <boost/enable_shared_from_this.hpp>
+
+#include <stdio.h>
+
---
+++ FTP/main.cpp
@@ -0,0 +1,26 @@
+//
+//  main.cpp
+//  FTP
+//
+//  Created by Daniil Vinogradov on 23.05.2018.
+//  Copyright © 2018 NoNameDude. All rights reserved.
+//
+
+#include "stdafx.h"
+
+#include "ftp_server.h"
+
+int main() {
+	try {
+		boost::asio::io_service io_service;
+		orianne::ftp_server server(io_service, 8080, "/Users/xitrix/Library/Developer/Xcode/DerivedData/FTP-aikctagvjyarurerixflmfpxdojb/Build");
+		io_service.run();
+		
+	}
+	catch(std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+	
+	return 0;
+}
